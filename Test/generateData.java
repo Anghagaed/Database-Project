964a5701bdf361bc42bd160f09b97818381a1f8f
qshah <FATIMA@FATIMA.local>
2016-11-12 23:33:52 -0800

    fatima's attempt for select queryin generateData.java

--- Test/generateData.java
+++ Test/generateData.java
@@ -1,5 +1,7 @@
 package database_Connection;
 
+import java.sql.ResultSet;
+
 import database_Connection.DBConnection;
 
 //import java.sql.*;
@@ -16,10 +18,10 @@ public class generateData {
 	}
 	
 	public void generateTuser() {
-		//sql = "INSERT INTO user (u_userid, u_username, u_cipher) VALUES (?, ?, ?);";
-		sql = "SELECT * FROM user WHERE u_id=? AND u_username=?;";
+		sql = "INSERT INTO user (u_userid, u_username, u_cipher) VALUES (?, ?, ?);";
+		//sql = "SELECT * FROM user WHERE u_id=?;";
 		dat.prepStmt(sql);
-		/*
+		
 		dat.bindIntStmt(1, 1);
 		dat.bindStringStmt("testUser1", 2);
 		dat.bindStringStmt("abcdefg", 3);
@@ -32,9 +34,19 @@ public class generateData {
 			dat.bindStringStmt("abcdefg" + i, 3);
 			dat.executeUpdateSQL();
 		} 
-		*/
-		dat.bindIntStmt(15, 1);
+		
+		/*dat.bindIntStmt(15, 1);
 		ResultSet table = dat.executeSQL();
+		
+		while(table.next())
+		{
+			//retrieve by column name
+			ind id = table.getInt("u_id");
+			//display vals
+			System.out.println("USER_ID: " + id);
+		}
+		
+		table.close();*/
 	}
 	
 	
