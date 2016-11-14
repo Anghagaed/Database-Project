fb1d20c1aa7845d53cf496c05c17628a483ad5ea
Anghagaed <hliang5@ucmerced.edu>
2016-11-13 18:09:05 -0800

    Poplatated from population 1 SQL

--- Population1.sql
+++ Population1.sql
@@ -61,66 +61,66 @@ INSERT INTO encryptedwallet (ew_encryptid, ew_id)
 VALUES (10, 10);
 
 /* encryptednote */
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (1, 1);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (2, 2);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (4, 4);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (3, 3);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (5, 5);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (6, 6);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (7, 7);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (8, 8);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (9, 9);
 
-INSERT INTO encryptedwallet (en_encryptid, en_id) 
+INSERT INTO encryptednote (en_encryptid, en_id) 
 VALUES (10, 10);
 
 /* encryptedpasswordentry */
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (1, 1);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (2, 2);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (4, 4);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id)  
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id)  
 VALUES (3, 3);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id)  
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id)  
 VALUES (5, 5);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (6, 6);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (7, 7);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (8, 8);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (9, 9);
 
-INSERT INTO encryptedwallet (epe_encryptid, epe_id) 
+INSERT INTO encryptedpasswordentry (epe_encryptid, epe_id) 
 VALUES (10, 10);
 
 /* passwordentry */
