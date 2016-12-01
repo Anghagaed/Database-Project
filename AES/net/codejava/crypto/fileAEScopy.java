1565f7fa1dcf9d6c8d6965a024a4ee7c91e4fe38
qshah <FATIMA@FATIMA.local>
2016-11-30 16:40:08 -0800

    fatima's aes

---
+++ AES/net/codejava/crypto/fileAEScopy.java
@@ -0,0 +1,92 @@
+package net.codejava.crypto;
+
+ 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+ 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+ 
+/**
+ * A utility class that encrypts or decrypts a file.
+ * @author www.codejava.net
+ *
+ */
+
+//hello this is fatima's attempt at tweaking the fileAES.java so it's a copy
+
+public class fileAEScopy {
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES";
+ 
+    public static void encrypt(String key, FileInputStream inputFile, File outputFile)
+            throws CryptoException {
+        doCryptoEncrypt(Cipher.ENCRYPT_MODE, key, inputFile, outputFile);
+    }
+ 
+    public static void decrypt(String key, File inputFile, FileOutputStream outputFile)
+            throws CryptoException {
+        doCryptoDecrypt(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
+    }
+ 
+    private static void doCryptoEncrypt(int cipherMode, String key, FileInputStream inputFile,
+            File outputFile) throws CryptoException {
+        try {
+            Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(cipherMode, secretKey);
+             
+            //FileInputStream inputStream = new FileInputStream(inputFile);
+            
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+             
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+             
+            //FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(outputBytes);
+             
+            inputStream.close();
+            outputStream.close();
+             
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex) {
+            throw new CryptoException("Error encrypting/decrypting file", ex);
+        }
+    }
+    
+    private static void doCryptoDecrypt(int cipherMode, String key, File inputFile,
+            FileOutputStream outputFile) throws CryptoException {
+        try {
+            Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(cipherMode, secretKey);
+             
+           // FileInputStream inputStream = new FileInputStream(inputFile);
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+             
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+             
+            //FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(outputBytes);
+             
+            inputStream.close();
+            outputStream.close();
+             
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex) {
+            throw new CryptoException("Error encrypting/decrypting file", ex);
+        }
+    }
+}
