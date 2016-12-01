05b1d76b60a3bb1d0383b8eefaf65520af7b7f41
Anghagaed <hliang5@ucmerced.edu>
2016-11-30 17:40:37 -0800

    magical girl

--- Database/database_Connection/database.java
+++ Database/database_Connection/database.java
@@ -1,5 +1,7 @@
 package database_Connection;
 
+import java.sql.*;
+
 public class database {
 	DBConnection dat;
 	static final String path = "jdbc:sqlite:Test/pwMan.db";					// example: "jdbc:sqlite:test.db"
@@ -15,5 +17,20 @@ public class database {
 		dat = new DBConnection(path);
 		dat.createConnection();
 		sql = null;
+	}
+	
+	public void displayUserNPass(String domainname, int userID) {
+		sql = "SELECT p_domainusername, p_domainpassword FROM passwordentry WHERE p_domainname=? AND p_userid=?;";
+		dat.prepStmt(sql);
+		dat.bindStringStmt(domainname, 1);
+		dat.bindIntStmt(userID, 2);
+		ResultSet rs = dat.executeSQL();
+		
+		String username = rs.getString("p_domainusername");
+		String password = rs.getString("p_domainpassword");
+		
+		System.out.println("Username :" + username);
+		System.out.println("Passowrd :" + password);
+		dat.clearStatement();
 	}
 }
