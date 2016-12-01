15dd8965420718b13bab194f7180af0308c364aa
Anghagaed <hliang5@ucmerced.edu>
2016-12-01 10:13:05 -0800

    stuff

--- Database/database_Connection/database.java
+++ Database/database_Connection/database.java
@@ -19,7 +19,7 @@ public class database {
 		sql = null;
 	}
 	
-	public void displayUserNPass(String domainname, int userID) {
+	public void displayUserNPass(String domainname, int userID) throws SQLException {
 		sql = "SELECT p_domainusername, p_domainpassword FROM passwordentry WHERE p_domainname=? AND p_userid=?;";
 		dat.prepStmt(sql);
 		dat.bindStringStmt(domainname, 1);
@@ -31,6 +31,62 @@ public class database {
 		
 		System.out.println("Username :" + username);
 		System.out.println("Passowrd :" + password);
+		rs.close();
 		dat.clearStatement();
+	}
+
+	public int insertPWEntry(int id, String domainname, int userID, String username, String passw) {
+		sql = "INSERT INTO passwordentry" + 
+				" (p_id, p_domainname, p_userid, p_domainusername, p_domainpassword)" +
+				" VALUES (?, ?, ?, ?, ?);";
+		dat.prepStmt(sql);
+		dat.bindIntStmt(id, 1);
+		dat.bindStringStmt(domainname, 2);
+		dat.bindIntStmt(userID, 3);
+		dat.bindStringStmt(username, 4);
+		dat.bindStringStmt(passw, 5);
+		int result = dat.executeUpdateSQL();
+		dat.clearStatement();
+		return result;
+	}
+	
+	public int deletePWEntry(int id, int userID) {
+		sql = "DELETE FROM passwordentry WHERE p_id=? AND p_userid=?;";
+		dat.prepStmt(sql);
+		dat.bindIntStmt(id, 1);
+		dat.bindIntStmt(userID, 2);
+		int result = dat.executeUpdateSQL();
+		dat.clearStatement();
+		return result;
+	}
+	
+	public int deleteWallet(int id, int userID) {
+		sql = "DELETE FROM wallet WHERE w_id=? AND w_userid=?;";
+		dat.prepStmt(sql);
+		dat.bindIntStmt(id, 1);
+		dat.bindIntStmt(userID, 2);
+		int result = dat.executeUpdateSQL();
+		dat.clearStatement();
+		return result;
+	}
+	
+	public int deleteNote(int id, int userID) {
+		sql = "DELETE FROM notes WHERE n_id=? AND n_userid=?;";
+		dat.prepStmt(sql);
+		dat.bindIntStmt(id, 1);
+		dat.bindIntStmt(userID, 2);
+		int result = dat.executeUpdateSQL();
+		dat.clearStatement();
+		return result;
+	}
+	
+	public int deleteAccount(int id, int userID) {
+		sql = "DELETE FROM accountinfo WHERE ai_id=? AND ai_userid=?;";
+		dat.prepStmt(sql);
+		dat.bindIntStmt(id, 1);
+		dat.bindIntStmt(userID, 2);
+		int result = dat.executeUpdateSQL();
+		dat.clearStatement();
+		return result;
 	}
 }
--- Database/database_Connection/test.java
+++ Database/database_Connection/test.java
@@ -1,18 +1,17 @@
 package database_Connection;
+import java.sql.*;
 
 public class test {
-	/*
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws SQLException {
 		try {
 			Class.forName("org.sqlite.JDBC");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		generateData dat = new generateData();
-		//dat.generateTuser();
-		dat.close();
+		database dat = new database();
+		dat.displayUserNPass("www.google.com", 1);
 	}
-	*/
 
 }
