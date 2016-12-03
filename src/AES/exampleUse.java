ec282f676f481ebc74519f1797b902364c4b092c
qshah <FATIMA@FATIMA.local>
2016-12-03 11:36:58 -0800

    working AES.java

---
+++ src/AES/exampleUse.java
@@ -0,0 +1,22 @@
+package AES;
+import java.util.Scanner;
+
+public class exampleUse {
+
+	public static void main(String[] args) 
+    {
+        final String secretKey = "hellotherehowu";
+         
+        //String originalString = "insertStringHere!";
+        Scanner scanner  = new Scanner (System.in);
+        String originalString = scanner.nextLine();
+        if(scanner != null) { scanner.close(); }
+        String encryptedString = AES.encrypt(originalString, secretKey) ;
+        String decryptedString = AES.decrypt(encryptedString, secretKey) ;
+         
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
+
+}
---
+++ src/AES/net/codejava/crypto/Test.java
@@ -0,0 +1,34 @@
+package AES.net.codejava.crypto;
+
+
+import java.io.*;
+ 
+/**
+ * A tester for the CryptoUtils class.
+ * @author www.codejava.net
+ *
+ */
+public class Test {
+    public static void main(String[] args) {
+        String key = "Mary has one cat";
+      
+        //code below is original code
+       File inputFile = new File("~/Desktop/DATABASE/test.db");
+        File encryptedFile = new File("document.encrypted");
+        File decryptedFile = new File("document.decrypted");
+        
+        //code below is fatima's attempt
+       /* FileInputStream inputFile = new FileInputStream("document.txt");
+        File encryptedFile = new File("document.encrypted");
+        FileOutputStream decryptedFile = new FileOutputStream("document.decrypted");*/
+        
+         
+        try {
+            fileAES.encrypt(key, inputFile, encryptedFile);
+            fileAES.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
